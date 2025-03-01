6d0ade2bc401a3f69346dacd05110e38fc87475c
JUAN GOMEZ <juandiego.gomez1@utp.edu.co>
2025-03-01 12:57:42 -0500

    se realizo la funcion box-muller

--- Actividad 3/Codigo python/metodos.py
+++ Actividad 3/Codigo python/metodos.py
@@ -1,7 +1,20 @@
+import math
+import random
+
+def boxMuller(mu= 0, sigma=1):
+    u1 = random.random()
+    u2 = random.random()
+
+    z0 = math.sqrt(-2 * math.log(u1)) * math.cos(2 * math.pi * u2)
+    z1 = math.sqrt(-2 * math.log(u1)) * math.sin(2 * math.pi * u2)
+
+    z0 = sigma * z0 + mu
+    z1 = sigma * z1 + mu
+
+    return z0, z1
 
 
-def boxMuller():
-    print("box muller")
+
 
 def rungeKutta4():
     print("Metodod de runge-kutta 4")
