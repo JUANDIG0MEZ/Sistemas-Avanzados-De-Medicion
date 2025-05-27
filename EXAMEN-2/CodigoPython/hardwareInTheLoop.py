13fe4bb4b50e000173846656fb9a97a335ea1467
JUAN GOMEZ <juandiego.gomez1@utp.edu.co>
2025-05-27 16:44:26 -0500

    Se mejoro el codigo

--- EXAMEN-2/CodigoPython/hardwareInTheLoop.py
+++ EXAMEN-2/CodigoPython/hardwareInTheLoop.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 def generar_fuerza(tiempo, amplitud, freq, ruido = 0.05):
     return amplitud * np.sin(2 * np.pi * freq * tiempo) + np.random.normal(0.0, ruido,len(tiempo))
 
+def generar_u(tiempo, amplitud, freq, ruido= 0.05):
+    return amplitud * np.sin(2 * np.pi * freq * tiempo) + np.random.normal(0.0, ruido,len(tiempo))
+
 class SegundoOrden:
     def __init__(self, omega_n, zeta, Ks, masa, dt):
         self.omega_n = omega_n
@@ -16,60 +19,117 @@ class SegundoOrden:
         self.x2 = 0.0 # dy(t)/dt
 
     def Actualizar(self, fuerza):
+
         x1 = self.x1 + self.dt * self.x2
         x2 = self.x2 + self.dt * (-(self.omega_n ** 2) * self.x1 - 2 * self.zeta * self.omega_n * self.x2 + self.Ks * fuerza / self.masa)
 
+        # Actualiza
         self.x1 = x1
         self.x2 = x2
 
         return x1, x2
     
-def graficar_señales(tiempo, fuerza, x1_vector, x2_vector, save=False):
-    import matplotlib.pyplot as plt
+class PrimerOrden:
+    def __init__(self, tau, k_s, dt):
+        self.tau = tau
+        self.k_s = k_s
+        self.dt = dt
+        self.x1 = 0.0
 
-    plt.figure()
-    plt.plot(tiempo, fuerza, label='Fuerza')
-    plt.plot(tiempo, x1_vector, label='x1(t)=y(t)')
-    plt.plot(tiempo, x2_vector, label='x2(t)', color='limegreen')
-    plt.title('Modelo de Segundo Orden')
-    plt.xlabel('Tiempo (s)')
-    plt.ylabel('Respuesta')
-    plt.legend()
-    plt.grid()
-    if save:
-        plt.savefig('./imagenes/SegundoOrden.png')
-    plt.show()
+    
+    def actualizar(self, u):
+        x1 = self.x1 + self.dt * ( self.k_s * u -self.x1)/ self.tau
+
+        # Actualiza
+        self.x1 = x1
+
+        return self.x1
     
 
 
-omega_n = 2.0
-zeta = 0.01
-k_s = 4.0
-masa = 1.0
+class Parametros_2:
+    omega_n = 2.0
+    zeta = 0.01
+    k_s = 4.0
+    masa = 1.0
+    
+    amplitud = 0.8
+    freq = 0.1
 
-# Parámetros de la simulación
-dt = 0.01  # segundos
-Tmax = 15  # segundos
-muestras = int(Tmax / dt)
-tiempo = np.linspace(0, Tmax, muestras + 1)
+    dt = 0.03
+    Tmax =10
+    muestras = int(Tmax / dt)
 
-# Parámetros de la señal de entrada Fuerza(t)
-amplitud = 0.8  # V
-freq = 1.5  # Hz
 
-fuerza = generar_fuerza(tiempo, amplitud, freq)
+class Parametros_1:
+    tau = 1.0
+    k_s = 1.0
+
+    amplitud= 0.8
+    freq = 0.1
+
+    dt = 0.02
+    Tmax =10
+    muestras = int(Tmax / dt)
+
+
+
+#######################################################################
+####################### MODELO PRIMER ORDEN ###########################
+#######################################################################
+
+tiempo_1 = np.linspace(0, Parametros_1.Tmax, Parametros_1.muestras + 1)
+u_1 = generar_u(tiempo_1, Parametros_1.amplitud, Parametros_1.freq)
+
+x1_modelo1 = []
+
+# Aplicar la entrada al modelo de primer orden
+modelo_1 = PrimerOrden(Parametros_1.tau, Parametros_1.k_s, Parametros_1.dt)
+
+for u in u_1:
+    x1 = modelo_1.actualizar(u)
+    x1_modelo1.append(x1)
+
+
+plt.figure()
+plt.plot(tiempo_1, u_1, label='Fuerza')
+plt.plot(tiempo_1, x1_modelo1, label='x1(t)=y(t)')
+plt.title('Modelo de Primer Orden')
+plt.xlabel('Tiempo (s)')
+plt.ylabel('Respuesta')
+plt.legend()
+plt.grid()
+plt.savefig('PrimerOrden.png')
+plt.show()
+
+
+#######################################################################
+######################## MODELO SEGUNDO ORDEN #########################
+#######################################################################
+
+tiempo_2 = np.linspace(0, Parametros_2.Tmax, Parametros_2.muestras + 1)
+fuerza_2 = generar_fuerza(tiempo_2, Parametros_2.amplitud, Parametros_2.freq)
 
 # Estado inicial
-x1_vector = []
-x2_vector = []
+x1_modelo2 = []
+x2_modelo2 = []
 
 # Aplicar la entrada al modelo de segundo orden
-modelo = SegundoOrden(omega_n, zeta, k_s, masa, dt)
+modelo_2 = SegundoOrden(Parametros_2.omega_n, Parametros_2.zeta, Parametros_2.k_s, Parametros_2.masa, Parametros_2.dt)
 
-for fz in fuerza:
-    x1, x2 = modelo.Actualizar(fz)
-    x1_vector.append(x1)
-    x2_vector.append(x2)
+for fz in fuerza_2:
+    x1, x2 = modelo_2.Actualizar(fz)
+    x1_modelo2.append(x1)
+    x2_modelo2.append(x2)
 
-# graficar las señales
-graficar_señales(tiempo, fuerza, x1_vector, x2_vector, save=True)
+plt.figure()
+plt.plot(tiempo_2, fuerza_2, label='Fuerza')
+plt.plot(tiempo_2, x1_modelo2, label='x1(t)=y(t)')
+plt.plot(tiempo_2, x2_modelo2, label='x2(t)', color='limegreen')
+plt.title('Modelo de Segundo Orden')
+plt.xlabel('Tiempo (s)')
+plt.ylabel('Respuesta')
+plt.legend()
+plt.grid()
+plt.savefig('SegundoOrden.png')
+plt.show()
--- EXAMEN-2/CodigoPython/main.py
+++ EXAMEN-2/CodigoPython/main.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from hardwareInTheLoop import x1_vector, x2_vector, tiempo, fuerza
-from hardwareInTheLoop import omega_n, zeta, k_s, masa, dt
+from hardwareInTheLoop import Parametros_2, x1_modelo2, x2_modelo2, tiempo_2, fuerza_2
+from hardwareInTheLoop import Parametros_1, x1_modelo1, tiempo_1, u_1
+
+
+
 class KalmanFilter:
     def __init__(self, F, H, Q, R, P0):
         self.F = F  # Matriz de transición
@@ -9,7 +12,7 @@ class KalmanFilter:
         self.Q = Q  # Covarianza del proceso
         self.R = R  # Covarianza del sensor
         self.P = P0  # Covarianza del error
-        self.x = np.array([[0.0], [0.0]])  # Estado inicial
+        self.x = np.array([[0.0], [0.0], [0.0]])  # Estado inicial
 
     def predict(self):
         self.x = self.F @ self.x
@@ -18,89 +21,142 @@ class KalmanFilter:
     def update(self, z):
         K = self.P @ self.H.T @ np.linalg.inv(self.H @ self.P @ self.H.T + self.R)
         self.x += K @ (z - self.H @ self.x)
-        self.P = (np.eye(2) - K @ self.H) @ self.P
+        self.P = (np.eye(3) - K @ self.H) @ self.P
 
 
+
+class Modelo2:
+    # Varianza de la tasa de cambio de la fuerza 
+    q_F = 0.001
+    # Varianza en las mediciones del sensor
+    r_F = 0.0003
+    # Covarianza inicial
+    P0 = np.eye(3) * 0.001  
+
+
+    # Matrices necesarias para kalman
+    A = np.array([
+        [0, 1, 0],
+        [-Parametros_2.omega_n**2, -2*Parametros_2.zeta*Parametros_2.omega_n, Parametros_2.k_s/ Parametros_2.masa],
+        [0, 0, 0]
+        ])
+    F = np.eye(3) + A * Parametros_2.dt
+    H = np.array([[1.0, 0.0, 0.0]])
+    Q = np.array([
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, q_F]]) * Parametros_2.dt
+    R = np.array([[r_F]])  # varianza del sensor
+
+
+    
+
+
+class Modelo1:
+    # Varianza de la tasa de cambio de u(t)
+    q_u = 0.001
+    # Varianza en las mediciones del sensor
+    r_u = 0.003
+
+    A = np.array([
+        [-1/Parametros_1.k_s, Parametros_1.k_s/Parametros_1.tau],
+        [0, 0]
+    ])
+
+    F = np.eye(2) + A *Parametros_1.dt
+    H = np.array([[1.0, 0.0]])
+    Q = np.array([
+        [0, 0],
+        [0, q_u * Parametros_1.dt]
+    ])
+    R = np.array([[r_u]])
 
 
 if __name__ == "__main__":
-    F_continua = np.array([[0, 1], [-omega_n**2, -2*zeta*omega_n]])  # Matriz de transición continua
-    F = np.eye(2) + F_continua * dt  # Matriz de transición discreta
-
-    B_continua = np.array([[0], [k_s/masa]])  # Matriz de entrada continua
-    B = B_continua * dt  # Matriz de entrada discreta
-
-    H = np.array([[1.0, 0.0]])  # Matriz de medición
-    Q = np.eye(2) * 5.0  # Covarianza del proceso
-    R = np.array([[0.05]])  # Covarianza del sensor
-    P0 = np.eye(2) * 1.0  # Covarianza inicial
     
+
+    ##################################################################
+    ###################### MODELO PRIMER ORDEN #######################
+    ##################################################################
+
+
+    # kf_1 = KalmanFilter(
+    #     Modelo1.F,
+    #     Modelo1.H,
+    #     Modelo1.Q,
+    #     Modelo1.R,
+    #     Modelo1.P0
+    # )
+
+    # x1_kalman_1 = []
+    # u_kalman = []
+
+    # for i in range(len(Parametros_1.tiempo)):
+    #     z = Parametros_1.x1_vector[i] + np.random.normal(0, Modelo1.r_u ** (1/2))
+
+    #     kf_1.predict()
+    #     kf_1.update(np.array([[z]]))
+
+
+    #     x1_kalman_1.append(kf_1.x[0, 0])
+    #     u_kalman.append([1, 0])
+
+
+
+    ##################################################################
+    ####################### MODELO SEGUNDO ORDEN #####################
+    ##################################################################
+
+
+    kf_2 = KalmanFilter(
+        Modelo2.F,
+        Modelo2.H,
+        Modelo2.Q,
+        Modelo2.R,
+        Modelo2.P0)
+
+    x1_kalman_2 = []
+    x2_kalman_2 = []
+    fuerza_kalman = []
     
-    kf = KalmanFilter(F, H, Q, R, P0)
+    for i in range(len(tiempo_2)):
 
-    x1_kalman = []
-    x2_kalman = []
-    
-    for i in range(len(tiempo)):
+        z = x1_modelo2[i] #+ np.random.normal(0, Modelo2.r_F ** (1/2))
 
-        z = x1_vector[i]
-
-        kf.predict() # Paso de prediccion
-        kf.update(np.array([[z]])) # z debe ser un vector columna y representa la meedicion de x1(t)
+        kf_2.predict() # Paso de prediccion
+        kf_2.update(np.array([[z]])) # z debe ser un vector columna y representa la meedicion de x1(t)
 
 
-        x1_kalman.append(kf.x[0, 0]) # x1 estimado 
-        x2_kalman.append(kf.x[1, 0]) # x2 estimado
-
-    
-    def recuperar_fuerza(x1, x2, dt, omega_n, zeta, k_s, masa):
-        # Convertir las listas a arrays de numpy
-        x1 = np.array(x1)
-        x2 = np.array(x2)
-        dx2 = np.gradient(x2, dt)
-
-        F_estimada = (masa / k_s) * (dx2 + omega_n**2 * x1 + 2.0 * zeta * omega_n * x2)
-        return F_estimada
-    
-    
-    fuerza_kalman = recuperar_fuerza(x1_kalman, x2_kalman, dt, omega_n, zeta, k_s, masa)
+        x1_kalman_2.append(kf_2.x[0, 0]) # x1 estimado 
+        x2_kalman_2.append(kf_2.x[1, 0]) # x2 estimado
+        fuerza_kalman.append(kf_2.x[2,0])
 
 
-    plt.plot(tiempo, x1_vector, label='x1(t)')
-    plt.plot(tiempo, x1_kalman, label='x1(t) estimado', color='limegreen')
+    plt.plot(tiempo_2, x1_modelo2, label='x1(t)')
+    plt.plot(tiempo_2, x1_kalman_2, label='x1(t) estimado')
     plt.xlabel('Tiempo (s)')
     plt.ylabel('Respuesta')
     plt.legend()
     plt.grid()
-    plt.savefig('./imagenes/EstimacionX1.png')
+    plt.savefig('EstimacionX1.png')
     plt.show()
 
 
-    plt.plot(tiempo, x2_vector, label='x2(t)')
-    plt.plot(tiempo, x2_kalman, label='x2(t) estimado', color='limegreen')
+    plt.plot(tiempo_2, x2_modelo2, label='x2(t)')
+    plt.plot(tiempo_2, x2_kalman_2, label='x2(t) estimado')
     plt.xlabel('Tiempo (s)')
     plt.ylabel('Respuesta')
     plt.legend()
     plt.grid()
-    plt.savefig('./imagenes/EstimacionX2.png')
+    plt.savefig('EstimacionX2.png')
     plt.show()
 
 
-    plt.plot(tiempo, fuerza, label='Fuerza')
-    plt.plot(tiempo, fuerza_kalman, label='Fuerza estimada', color='limegreen')
+    plt.plot(tiempo_2, fuerza_2, label='Fuerza')
+    plt.plot(tiempo_2, fuerza_kalman, label='Fuerza estimada')
     plt.xlabel('Tiempo (s)')
     plt.ylabel('Respuesta')
     plt.legend()
     plt.grid()
-    plt.savefig('./imagenes/EstimacionF.png')
+    plt.savefig('EstimacionF.png')
     plt.show()
-
-    plt.plot(tiempo, x2_kalman, label='x2(t) estimado')
-    plt.plot(tiempo, np.gradient(x1_kalman, dt), label='x2(t) estimado', color='limegreen')
-    plt.xlabel('Tiempo (s)')
-    plt.ylabel('Respuesta')
-    plt.legend()
-    plt.grid()
-    plt.savefig('./imagenes/EstimacionX1Fuerza.png')
-    plt.show()
-
